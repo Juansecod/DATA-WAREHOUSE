0f1e174a1bb146d1f6df1c2a4da0120b08c234f4
Juansecod <juan.sebastianri28@gmail.com>
2021-08-30 18:23:29 -0500

    Cracion de la rama backend para la realizacion de este diferenciado del frontend. Actualizacion de archivos.

--- routers/regions.routers.js
+++ routers/regions.routers.js
@@ -4,7 +4,21 @@ const regionsControllers = require('../controllers/regions.controllers');
 const middlewares = require('../middleware/middlewares');
 
 router.use(middlewares.tokenValidator, middlewares.adminIdentificator);
+// Regiones
 router.get('/', regionsControllers.getRegions);
-router.put('/update/:id', regionsControllers.updateRegion);
+router.post('/register', regionsControllers.postRegion);
+router.put('/update/:idRegion', regionsControllers.updateRegion);
+router.delete('/delete', regionsControllers.deleteRegion);
+
+// Paises
+router.get('/countries', regionsControllers.getCountries);
+router.get('/:idRegion/countries', regionsControllers.getCountriesOfRegion);
+router.post('/:idRegion/countries/register', regionsControllers.postCountriesOfRegion);
+router.put('/:idRegion/countries/update/:idCountry', regionsControllers.updateCountry);
+router.delete('/:idRegion/countries/delete', regionsControllers.deleteCountry);
+
+// Ciudades
+router.get('/cities', regionsControllers.getCities);
+router.get('/countries/:idCountry/cities', regionsControllers.getCitiesesOfCountry);
 
 module.exports = router;
