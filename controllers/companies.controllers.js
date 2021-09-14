0fc91c65f8f9783f29b52a9bca70202f4cfd266e
Juansecod <juan.sebastianri28@gmail.com>
2021-09-14 17:15:37 -0500

    Creacion de una mejor arquitectura del proyecto en la rama principal

--- controllers/companies.controllers.js
+++
@@ -1,5 +0,0 @@
-/* Conexion BD */
-const sequelize = require('../config/conexion.js');
-
-
-module.exports = {};
--- routers/companies.routers.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const companiesControllers = require('../controllers/companies.controllers');
-
-module.exports = router;
