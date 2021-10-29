bda7461a43729e9cc91ce0cdd75d4c0f7a32991f
Juan Sebastian Rios Valencia <juan.sebastianri28@gmail.com>
2021-10-28 23:22:57 -0500

    DiseÃño formulario registrar contacto terminado

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -5,7 +5,7 @@ const admin = localStorage.getItem('admin');
 /* Diseño de la navbar default, analizando los requrimientos del usuario */
 const navbar = `<h2 class="logo">Logo</h2>
 <ul class="menu">
-  ${token ? '<li class="menu--contacts"><a href="">Contactos</a></li>' : ""}
+  ${token ? '<a href="index.html"><li class="menu--contacts">Contactos</li></a>' : ""}
   ${token && admin === "true" ? '<li class="menu--users"><a href="">Usuarios</a></li>' : ""}
   ${token ? '<li class="menu--companies"><a href="">Compañias</a></li>' : ""}
   ${token && admin === "true" ? '<li class="menu--regions"><a href="">Región/Ciudad</a></li>' : ""}
