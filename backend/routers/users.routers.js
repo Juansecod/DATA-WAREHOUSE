dc0a3a0e73ebb4509316f90fa893743a01355459
Juan Sebastian <juan.sebastianri28@gmail.com>
2021-11-02 16:34:42 -0500

    Correccion rutas al registrar y loggear un usuario

--- backend/routers/users.routers.js
+++ backend/routers/users.routers.js
@@ -7,7 +7,7 @@ const usersControllers = require('../controllers/users.controllers');
 const middlewares = require("../middleware/middlewares");
 
 /* Rutas */
-router.post('/signUp', middlewares.emailValidator, usersControllers.singupUser);
+router.post('/signIp', middlewares.emailValidator, usersControllers.singupUser);
 
 /* Acceso solo Admin */
 router.use(middlewares.tokenValidator, middlewares.adminIdentificator);
@@ -15,7 +15,7 @@ router.get('/', usersControllers.getUSers);
 router.get('/:id', usersControllers.getUSer);
 router.put('/update/:id', usersControllers.updateUser);
 router.put('/updateRol/:idUsuario', usersControllers.updateRolUser);
-router.post('/signinUser', middlewares.emailValidator, middlewares.passwordEncrypt, usersControllers.singinUser);
+router.post('/signUpUser', middlewares.emailValidator, middlewares.passwordEncrypt, usersControllers.singinUser);
 router.delete('/delete', usersControllers.deleteUser);
 
 module.exports = router;
