627a92ae4db605f4ebce76fd0ee8b94228cf748c
Juan Sebastian <juan.sebastianri28@gmail.com>
2021-11-02 16:36:50 -0500

    Update users.routers.js

--- backend/routers/users.routers.js
+++ backend/routers/users.routers.js
@@ -7,7 +7,7 @@ const usersControllers = require('../controllers/users.controllers');
 const middlewares = require("../middleware/middlewares");
 
 /* Rutas */
-router.post('/signIn', middlewares.emailValidator, usersControllers.singupUser);
+router.post('/signIn', middlewares.emailValidator, usersControllers.singInUser);
 
 /* Acceso solo Admin */
 router.use(middlewares.tokenValidator, middlewares.adminIdentificator);
@@ -15,7 +15,7 @@ router.get('/', usersControllers.getUSers);
 router.get('/:id', usersControllers.getUSer);
 router.put('/update/:id', usersControllers.updateUser);
 router.put('/updateRol/:idUsuario', usersControllers.updateRolUser);
-router.post('/signUpUser', middlewares.emailValidator, middlewares.passwordEncrypt, usersControllers.singinUser);
+router.post('/signUpUser', middlewares.emailValidator, middlewares.passwordEncrypt, usersControllers.singUpUser);
 router.delete('/delete', usersControllers.deleteUser);
 
 module.exports = router;
