8d8cc6e956de9fe71dadaea2e41a4512b17b85f7
Juan Sebastian <juan.sebastianri28@gmail.com>
2021-11-20 20:20:53 -0500

    Correccion middleware en rutas get de regiones

--- backend/routers/regions.routers.js
+++ backend/routers/regions.routers.js
@@ -12,26 +12,26 @@ const middlewares = require('../middleware/middlewares');
 
 /* Rutas */
 router.get('/list', regionsControllers.getListRegions);
+router.get('/', regionsControllers.getRegions);
+router.get('/countries', countriesControllers.getCountries);
+router.get('/:idRegion/countries', countriesControllers.getCountriesOfRegion);
+router.get('/cities', citiesControllers.getCities);
+router.get('/countries/:idCountry/cities', citiesControllers.getCitiesesOfCountry);
 
 /* Acceso solo admin */
 router.use(middlewares.tokenValidator, middlewares.adminIdentificator);
 
-    // Regiones
-router.get('/', regionsControllers.getRegions);
+    // Regiones Admin
 router.post('/register', regionsControllers.postRegion);
 router.put('/update/:idRegion', regionsControllers.updateRegion);
 router.delete('/delete', regionsControllers.deleteRegion);
 
-    // Paises
-router.get('/countries', countriesControllers.getCountries);
-router.get('/:idRegion/countries', countriesControllers.getCountriesOfRegion);
+    // Paises Admin
 router.post('/:idRegion/countries/register', countriesControllers.postCountriesOfRegion);
 router.put('/countries/update/:idCountry', countriesControllers.updateCountry);
 router.delete('/countries/delete', countriesControllers.deleteCountry);
 
-    // Ciudades
-router.get('/cities', citiesControllers.getCities);
-router.get('/countries/:idCountry/cities', citiesControllers.getCitiesesOfCountry);
+    // Ciudades Admin
 router.post('/countries/:idCountry/cities/register', citiesControllers.postCitiesOfCountry);
 router.put('/cities/update/:idCity', citiesControllers.updateCity);
 router.delete('/cities/delete', citiesControllers.deleteCity);
